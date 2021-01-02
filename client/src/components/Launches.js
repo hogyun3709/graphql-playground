c20f29da3209341268640c819ef32753b59838f4
hogyun3709 <hogyun3709@gmail.com>
2021-01-02 21:36:47 +0900

    Write: Build Launch Query and LaunchItems to show Laucnh lists

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,29 +1,35 @@
 /* Make Query to graphql */
 
-import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo'
-
-const LAUNCHES_QUERY = gql `
- query LaunchesQuery {
-   launches {
-     flight_number
-     mission_name
-     launch_date_local
-     launch_success
-   }
- }
+import React, { Fragment } from "react";
+// import gql from 'graphql-tag';
+// import { Query } from 'react-apollo'
+import { gql, useQuery } from "@apollo/client";
+import LaunchItem from "./LaunchItem";
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+  }
 `;
 
-export class Launches extends Component {
-  render(){
-    return(
-      <div>
-        <h1 className="display-4 my-3">Launches</h1>
-      </div>
-    )
-  }
+function Launches({}) {
+  const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+  if (loading) return "Loading..";
+  if (error) console.log(error);
+  console.log(data);
 
+  return (
+    <Fragment>
+      <h1 className="display-4 my-3">Laucnhes</h1>
+      {data.launches.map(launch => (
+        <LaunchItem key={launch.flight_number} launch={launch} />
+      ))}
+    </Fragment>
+  );
 }
 
-export default Launches
+export default Launches;
